fd134fc608eb0eb10e236a9e18394b30bfd93ac8
Levi Kanwischer <levi@kanwischer.me>
2016-08-21 13:10:08 -0700

    Add minor formatting changes (no code functional impacts)

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ from setuptools import setup, find_packages
 
 
 NAME = 'tabtosql'
+HERE = os.path.dirname(os.path.abspath(__file__))
 
 
 def read(filename):
     """Load file contents from setup.py path."""
-    here = os.path.dirname(os.path.abspath(__file__))
-    filepath = os.path.join(here, filename)
+    filepath = os.path.join(HERE, filename)
+
     if not os.path.isfile(filepath):
         raise OSError('%s missing from `%s` source.' % (filename, NAME))
 
@@ -30,34 +31,37 @@ def version(baked):
     return match.group(1) if match else baked
 
 
-CLASSIFIERS = ['Development Status :: 5 - Production/Stable',
-               'Environment :: Console',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved :: MIT License',
-               'Natural Language :: English',
-               'Operating System :: OS Independent',
-               'Programming Language :: Python',
-               'Programming Language :: Python :: 2',
-               'Programming Language :: Python :: 2.6',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3',
-               'Programming Language :: Python :: 3.4',
-               'Programming Language :: Python :: 3.5',
-               'Topic :: Utilities',
-              ]
-
-
-setup(name=NAME,
-      version=version(baked='1.0.1'),
-      author='Levi Kanwischer',
-      author_email='levi@kanwischer.me',
-      license='MIT',
-      url='https://github.com/LeviKanwischer/%s' % NAME,
-      description=__doc__,
-      long_description=read('README.rst'),
-      classifiers=CLASSIFIERS,
-      keywords='Tableau SQL',
-      install_requires=['click>=4.1'],
-      entry_points={'console_scripts': ['tabtosql = tabtosql.__main__:cli']},
-      packages=find_packages(),
-     )
+setup(
+    name=NAME,
+    version=version(baked='1.0.3'),
+    author='Levi Kanwischer',
+    author_email='levi@kanwischer.me',
+    license='MIT',
+    url='https://github.com/LeviKanwischer/%s' % NAME,
+    description=__doc__,
+    long_description=read('README.rst'),
+    keywords='Tableau SQL',
+    install_requires=['click>=4.1'],
+    entry_points={
+        'console_scripts': [
+            'tabtosql = tabtosql.__main__:cli',
+        ]
+    },
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
+)
--- tabtosql/workbook.py
+++ tabtosql/workbook.py
@@ -26,17 +26,18 @@ LINE_SMALL = 50
 def return_xml(filename):
     """Load twb XML into memory & return root object."""
     _validate_file(filename)
+
     if filename.endswith('.twbx'):
         return _parse_twbx(filename)
+
     return ET.parse(filename).getroot()
 
 
 def _validate_file(filename):
     """Validate given file is acceptable for processing."""
-    # File path must exist
     if not os.path.isfile(filename):
         raise OSError('%s is not a valid file path.' % filename)
-    # Must be an approved tableau filetabto
+
     if filename.split('.')[-1] not in ('twb', 'twbx'):
         raise OSError('%s is not a valid tableau file.' % filename)
 
@@ -45,6 +46,7 @@ def _parse_twbx(filename):
     """Parse twbx zip & return twb XML."""
     with open(filename, 'rb') as infile:
         twbx = zipfile.ZipFile(infile)
+
         for item in twbx.namelist():
             if item.endswith('.twb'):
                 twb = twbx.open(item)
@@ -54,11 +56,13 @@ def _parse_twbx(filename):
 def parse_worksheets(worksheets):
     """Parse worksheet xml objects & return cleaned values."""
     results = OrderedDict()
+
     for worksheet in worksheets:
         name = worksheet.attrib['name']
         datasource = worksheet.find('table/view/datasources')
         datasource = [i.attrib['caption'] for i in datasource if 'caption' in i.attrib]
         results[name] = datasource
+
     return results
 
 
@@ -66,14 +70,19 @@ def parse_datasources(datasources):
     """Parse connection xml objects & return cleaned values."""
     results = OrderedDict()
     datasources = [i for i in datasources if 'caption' in i.attrib]
+
     for datasource in datasources:
+
         name = datasource.attrib['caption']
         datasource = datasource.find('connection')
+
         engine = datasource.attrib['class'] if 'class' in datasource.attrib else None
         database = datasource.attrib['dbname'] if 'dbname' in datasource.attrib else None
         server = datasource.attrib['server'] if 'server' in datasource.attrib else None
         username = datasource.attrib['username'] if 'username' in datasource.attrib else None
+
         results[name] = {'engine': engine, 'db': database, 'server': server, 'user': username}
+
     return results
 
 
@@ -81,14 +90,18 @@ def parse_queries(datasources):
     """Parse query&table xml objects & return cleaned values."""
     results = OrderedDict()
     datasources = [i for i in datasources if 'caption' in i.attrib]
+
     for datasource in datasources:
+
         name = datasource.attrib['caption']
         conn = datasource.find('connection/relation')
         query = conn.text if conn.text else '-- LINKED TO: %s' % conn.attrib['table']
-        # Cleans eval operators that are manipulated by tableau for XML
+
         query = query.replace('<<', '<').replace('>>', '>')
+
         # TODO: Should be handling for universal newlines better (ie \r\n)
         results[name] = re.sub(r'\r\n', r'\n', query)
+
     return results
 
 
@@ -97,22 +110,28 @@ def format_header(filename):
     filename = os.path.abspath(filename)
     username = getpass.getuser()
     today = datetime.now().strftime('%Y-%m-%d %I:%M%p')
+
     output = '%s\n' % ('-'*LINE_BIG)
     output += '-- Created by: %s\n' % username
     output += '-- Created on: %s\n' % today
     output += '-- Source: %s\n' % filename
     output += '%s%s' % ('-'*LINE_BIG, '\n'*3)
+
     return output
 
 
 def format_worksheets(worksheets):
     """Format worksheets object for outfile."""
     output = '-- Worksheets w/ Datasources %s\n' % ('-'*(LINE_BIG-29))
+
     for worksheet in worksheets:
         output += '-- %s\n' % worksheet
+
         for source in worksheets[worksheet]:
             output += '  -- %s\n' % source
+
         output += '\n'
+
     output += '\n'*2
     return output
 
@@ -120,6 +139,7 @@ def format_worksheets(worksheets):
 def format_datasources(datasources):
     """Format datasources object for outfile."""
     output = '-- Datasources & Connections %s\n' % ('-'*(LINE_BIG-29))
+
     for source in datasources:
         output += '-- %s\n' % source
         output += '  -- Server: %s\n' % datasources[source]['server']
@@ -127,16 +147,19 @@ def format_datasources(datasources):
         output += '  -- Database: %s\n' % datasources[source]['db']
         output += '  -- Username: %s\n' % datasources[source]['user']
         output += '\n'*2
+
     return output
 
 
 def format_queries(queries):
     """Format datasources object for outfile."""
     output = '-- Queries %s\n' % ('-'*(LINE_BIG-11))
+
     for query in queries:
         output += '-- %s %s\n' % (query, '-'*(LINE_SMALL-4-len(query)))
         output += queries[query]
         output += '\n;%s' % ('\n'*3)
+
     return output
 
 
@@ -152,4 +175,5 @@ def convert(filename):
     output += format_worksheets(worksheets)
     output += format_datasources(datasources)
     output += format_queries(sql)
+
     return output
