f2235d1cc72dccd9a3d695ac77d19aa68bedf4cb
Levi Kanwischer <levi@kanwischer.me>
2016-12-17 19:25:57 -0800

    Move CLI out of __main__ into it's own module

--- tabtosql/__main__.py
+++ tabtosql/__main__.py
@@ -16,29 +16,8 @@ valid sql & human readable format.
 See the README for further details.
 """
 
-import sys
-
-import click
-
-import tabtosql
+from tabtosql import cli
 
 
-@click.command()
-@click.argument('filename')
-def cli(filename):
-    """Tableau Workbook SQL Extract Tool
-
-    tabtosql is a command line tool for parsing sql queries & related
-    information out of tableau workbooks (.twb & .twbx files). It works by
-    taking a tableau workbook, parsing the xml, and formatting information
-    about worksheets, connections to those worksheets, their connection(db)
-    details, and the corresponding custom sql (assuming it exists) in a
-    valid sql & human readable format.
-
-        USAGE:
-
-            $ tabtosql input.twb(x) > output.sql
-
-    See the README for further details.
-    """
-    sys.stdout.write(tabtosql.convert(filename))
+if __name__ == '__main__':
+    cli.main()
---
+++ tabtosql/cli.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""tabtosql.cli
+
+This module contains the command line interface for `tabtosql`.
+
+
+See the README for further details.
+
+"""
+
+import sys
+
+import click
+
+from tabtosql.workbook import convert
+
+
+@click.command()
+@click.argument('filename')
+def main(filename):
+    """Tableau Workbook SQL Extract Tool
+
+    tabtosql is a command line tool for parsing sql queries & related
+    information out of tableau workbooks (.twb & .twbx files). It works by
+    taking a tableau workbook, parsing the xml, and formatting information
+    about worksheets, connections to those worksheets, their connection(db)
+    details, and the corresponding custom sql (assuming it exists) in a
+    valid sql & human readable format.
+
+        USAGE:
+
+            $ tabtosql input.twb(x) > output.sql
+
+    See the README for further details.
+    """
+    sys.stdout.write(tabtosql.convert(filename))
+
+
+if __name__ == '__main__':
+    main()
